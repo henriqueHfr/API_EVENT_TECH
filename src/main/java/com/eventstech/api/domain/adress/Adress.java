5dab47c6529f504c866b5dc27361c238b1772186
Henrique Rodrigues <henrique.hfr2006@gmail.com>
2024-09-05 20:21:42 -0300

    Feat: First commit for aplication event tech

---
+++ src/main/java/com/eventstech/api/domain/adress/Adress.java
@@ -0,0 +1,29 @@
+package com.eventstech.api.domain.adress;
+
+import com.eventstech.api.domain.event.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "adress")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Adress {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String city;
+    private String uf;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+}
---
+++ src/main/java/com/eventstech/api/domain/coupon/Coupon.java
@@ -0,0 +1,32 @@
+package com.eventstech.api.domain.coupon;
+
+
+import com.eventstech.api.domain.event.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Table(name = "coupon")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Coupon {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String code;
+    private Integer discount;
+    private Date date;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+}
